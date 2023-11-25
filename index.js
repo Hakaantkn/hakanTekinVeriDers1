7c9b6fa0df744224296fd4307efd25b8591448e7
Hakan Tekin <hak4n45@gmail.com>
2023-11-25 12:38:44 +0300

    let cons console length

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("hello world")
+// console.log("hello world")
+// String - STRİNG - str - Str
+let hello = "selam";
+const hello2 = "selam";
+const space = " ";
+console.log(hello + space + hello2)
+
+let text = "hakan";
+let length = text.length;   
+console.log(length)
