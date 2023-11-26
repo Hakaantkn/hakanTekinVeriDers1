4dcf22927baec33d2b7af0db66aa03a22b0f0794
Hakan Tekin <hak4n45@gmail.com>
2023-11-26 13:11:37 +0300

    while deney eklendi

--- loops.js
+++ loops.js
@@ -3,3 +3,23 @@
 //     console.log(i);
 //     i++
 // }
+
+let isim = "hakan";
+let dogumTarihi = 2008;
+let cinsiyet = "erkek";
+let sayac = 0;
+const date = new date();
+const dateYear = date.getFullYear();
+let yas = dateYear - dogumTarihi;
+
+while(sayac < 10){
+    if(cinsiyet == "erkek" && yas >= 20){
+      console.log(`${isim} kaçak hain askere git`);  
+    } else if(cinsiyet != "erkek") {
+        console.log(`${isim} askere gitme zorunluluğun yok`);
+        break;
+    } else {
+        console.log(`${isim} burda ne işin var`);
+        break;
+    }
+}
